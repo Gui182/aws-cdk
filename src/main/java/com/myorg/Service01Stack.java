034edebd59e492fb5d5eacb47e7403246a830155
Gui182 <guilherme.oliveira182@yahoo.com.br>
2024-12-05 19:37:05 -0300

    feat: Add RDS service

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -1,6 +1,10 @@
 package com.myorg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Fn;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -18,53 +22,63 @@ import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 public class Service01Stack extends Stack {
-    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
-        this(scope, id, null, cluster);
-    }
+        public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+                this(scope, id, null, cluster);
+        }
 
-    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
-        super(scope, id, props);
+        public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+                super(scope, id, props);
 
-        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
-                .create(this, "ALB01")
-                .serviceName("service-01")
-                .cluster(cluster)
-                .cpu(512)
-                .desiredCount(2)
-                .listenerPort(8080)
-                .assignPublicIp(true)
-                .memoryLimitMiB(1024)
-                .taskImageOptions(
-                        ApplicationLoadBalancedTaskImageOptions.builder()
-                                .containerName("aws_project01")
-                                .image(ContainerImage.fromRegistry("gui182/aws_java_project01"))
-                                .containerPort(8080)
-                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
-                                        .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
-                                                .logGroupName("Service01")
-                                                .removalPolicy(RemovalPolicy.DESTROY)
-                                                .build())
-                                        .streamPrefix("Service01")
-                                        .build()))
-                                .build())
-                .publicLoadBalancer(true)
-                .build();
+                Map<String, String> envVariables = new HashMap<>();
+                envVariables.put("SPRING_DATASOURCE_URL", "jdbc:mariadb://" + Fn.importValue("rds-endpoint")
+                                + "3306/aws_project01?createDatabaseIfNotExist=true");
+                envVariables.put("SPRING_DATASOURCE_USERNAME", "admin");
+                envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
 
-        service01.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
-                .path("/actuator/health")
-                .port("8080")
-                .healthyHttpCodes("200")
-                .build());
+                ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
+                                .create(this, "ALB01")
+                                .serviceName("service-01")
+                                .cluster(cluster)
+                                .cpu(512)
+                                .desiredCount(2)
+                                .listenerPort(8080)
+                                .assignPublicIp(true)
+                                .memoryLimitMiB(1024)
+                                .taskImageOptions(
+                                                ApplicationLoadBalancedTaskImageOptions.builder()
+                                                                .containerName("aws_project01")
+                                                                .image(ContainerImage.fromRegistry(
+                                                                                "gui182/aws_java_project01:1.0.0"))
+                                                                .containerPort(8080)
+                                                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                                                .logGroup(LogGroup.Builder.create(this,
+                                                                                                "Service01LogGroup")
+                                                                                                .logGroupName("Service01")
+                                                                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                                                                .build())
+                                                                                .streamPrefix("Service01")
+                                                                                .build()))
+                                                                .environment(envVariables)
+                                                                .build())
+                                .publicLoadBalancer(true)
+                                .build();
 
-        ScalableTaskCount scalableTaskCount = service01.getService().autoScaleTaskCount(EnableScalingProps.builder()
-                .minCapacity(2)
-                .maxCapacity(4)
-                .build());
+                service01.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+                                .path("/actuator/health")
+                                .port("8080")
+                                .healthyHttpCodes("200")
+                                .build());
 
-        scalableTaskCount.scaleOnCpuUtilization("Service01AutoScaling", CpuUtilizationScalingProps.builder()
-                .targetUtilizationPercent(50)
-                .scaleInCooldown(Duration.seconds(60))
-                .scaleOutCooldown(Duration.seconds(60))
-                .build());
-    }
+                ScalableTaskCount scalableTaskCount = service01.getService()
+                                .autoScaleTaskCount(EnableScalingProps.builder()
+                                                .minCapacity(2)
+                                                .maxCapacity(4)
+                                                .build());
+
+                scalableTaskCount.scaleOnCpuUtilization("Service01AutoScaling", CpuUtilizationScalingProps.builder()
+                                .targetUtilizationPercent(50)
+                                .scaleInCooldown(Duration.seconds(60))
+                                .scaleOutCooldown(Duration.seconds(60))
+                                .build());
+        }
 }
